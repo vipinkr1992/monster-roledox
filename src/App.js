b5565e6af5d0523021237dea8b1d81549d96533c
vipinkr1992 <vipinkr1992@proton.me>
2025-03-17 11:34:46 +0530

    Use React Hooks

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
-import {Component} from 'react';
+import {useEffect,useState} from 'react';
 import './App.css';
 import SearchBox from './components/search-box/search-box.component';
 import CardList from './components/card-list/card-list.component';
 
+const App = () => {
+  const [searchField,setSearchField] = useState('');
+  const [monsters,setMonsters] = useState([]);
+  const [filteredMonsters,setFilteredMonsters] = useState(monsters);
+  
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => setMonsters(users));
+  },[])
+
+  useEffect(()=>{
+    const filteredMonsters = monsters.filter((monster)=>{
+      return monster.name.toLowerCase().includes(searchField);
+    })
+    setFilteredMonsters(filteredMonsters)
+  },[monsters,searchField])
+
+  const onSearchChange = event => {
+    const searchFieldString = event.target.value.toLowerCase();
+    //this.setState({searchField:searchFieldString});
+    setSearchField(searchFieldString);
+  }
+
+  return(
+   <div className='App'>
+      <h1>Monsters Roledox</h1>
+      <SearchBox className='monsters-search-box' placeholder='Search Monsters' onChangeHandler={onSearchChange}/>
+      <CardList monsters={filteredMonsters} />
+       
+    </div>
+  )
+}
+/*
 class App extends Component {
  constructor(){
   super();
@@ -43,5 +77,5 @@ class App extends Component {
   )
  }
 }
-
+*/
 export default App;
